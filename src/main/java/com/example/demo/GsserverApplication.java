086a7a7ebcde62af091d0ba2023c85874bbe56ff
justin.adjassem@gmail.com <justin.adjassem@gmail.com>
2020-04-29 14:50:56 +0000

    GS

---
+++ src/main/java/com/example/demo/GsserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsserverApplication.class, args);
+	}
+
+}
